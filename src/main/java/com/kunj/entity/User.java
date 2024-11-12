be843e7a02b9ec5e97c08963f7e19947e7828444
Vashist10 <rajesh.vashist.01@gmail.com>
2024-11-12 09:13:55 +0530

    delete unused code:

--- src/main/java/com/kunj/entity/User.java
+++ src/main/java/com/kunj/entity/User.java
@@ -8,14 +8,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * The type User.
@@ -26,7 +22,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @AllArgsConstructor
 @Entity
 @Table(name = "pg_user")
-public class User  implements UserDetails {
+public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -58,38 +54,4 @@ public class User  implements UserDetails {
 
   @OneToOne(mappedBy = "user")
   private UserAuthToken userAuthToken;
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of();
-  }
-
-  @Override
-  public String getPassword() {
-    return "";
-  }
-
-  @Override
-  public String getUsername() {
-    return mobileNumber;
-  }
-
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
 }
